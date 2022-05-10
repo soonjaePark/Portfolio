97bd9746cb7b2e12a29c338ed31cfc95c429a289
sjpark <sjpark@thezak.co.kr>
2022-05-10 22:19:48 +0900

    no message

--- src/main/java/sjpark/porfolio/portfolio/controllers/MyInfoController.java
+++ src/main/java/sjpark/porfolio/portfolio/controllers/MyInfoController.java
@@ -3,7 +3,6 @@ package sjpark.porfolio.portfolio.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
